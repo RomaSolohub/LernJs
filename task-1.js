7aeede7d66152d59a35a6fe319ddec3f393f7ff3
RomaSolohub <roman30121998@gmail.com>
2021-07-29 15:05:12 +0300

    иф елсе

--- task-1.js
+++ task-1.js
@@ -5,6 +5,6 @@ let ya = 4/25;
 let er = 2/3; 
 let ii = ((ru+ua)/ya*er);
 let ig = (ru-ua-ya+er);
- jf(ii<ig)
-  {alert (GOOD)}
- else {alert (NoGood)};
+ if (ii<ig)
+  (alert ("good"))
+ else (alert ("nogood"));
