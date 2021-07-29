3f6d7b69d00fde0553987a910262d5f1e113eb93
RomaSolohub <roman30121998@gmail.com>
2021-07-29 16:34:31 +0300

    tt

--- task-1.js
+++ task-1.js
@@ -1,10 +1,3 @@
 
-let ru = 5;
-let ua = 3; 
-let ya = 4/25;
-let er = 2/3; 
-let ii = ((ru+ua)/ya*er);
-let ig = (ru-ua-ya+er);
- if (ii<ig)
-  (alert ("good"))
- else (alert ("nogood"));
+let nameEarth = "Земля"; 
+let userName = "Ivan";
