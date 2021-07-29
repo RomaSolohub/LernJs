ebd17f734e53ec45566a112233a3af23a470d32d
RomaSolohub <roman30121998@gmail.com>
2021-07-29 14:58:11 +0300

    Какойто текст описательный

--- task-1.js
+++ task-1.js
@@ -1,9 +1,10 @@
 
-let message;
-
-message = 'Не говори, що робив, а кажи що зробив! ';
-alert (message);
-message = 'Нажми ок, если хочешь чтобы Рома учил програмирование ';
-alert (message);
-message = 'Ха-Ха,Он уже учит. Иди работай а не на сайтах сиди) ';
-alert (message);
+let ru = 5;
+let ua = 3; 
+let ya = 4/25;
+let er = 2/3; 
+let ii = ((ru+ua)/ya*er);
+let ig = (ru-ua-ya+er);
+ jf(ii<ig)
+  {alert (GOOD)}
+ else {alert (NoGood)};
