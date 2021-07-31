1208a0cdb04c634f8fce0d7491278f8120c10779
RomaSolohub <roman30121998@gmail.com>
2021-07-31 22:01:49 +0300

    tera

--- task-1.js
+++ task-1.js
@@ -1,2 +1,8 @@
-let nameUser = prompt('What is your name?',"" );
-alert('Pizdish!')
+let a = 1, b = 1;
+
+let c = ++a; 
+let d = b++;
+alert ("a1="+a);
+alert ("b1="+b);
+alert ("c1="+c);
+alert ("d1="+d);
