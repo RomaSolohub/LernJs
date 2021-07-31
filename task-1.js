d0f0ef4b45630542d3a2e8989164fef43299a2db
RomaSolohub <roman30121998@gmail.com>
2021-07-31 23:38:27 +0300

    rew

--- task-1.js
+++ task-1.js
@@ -1,6 +1,9 @@
 // it's link for my site 
 // https://romasolohub.github.io/LernJs/
-
-
-let massage = prompt ('Сколько тебе лет?','');
-alert ("Пиздишь)")
+let hour = prompt(`Сколько сейчас часов?`,'')
+let messege = (0 < hour < 8) ? 'Магази н закрыт':
+(8 < hour < 13) ? 'Магазин открыт':
+(13 < hour < 15) ? 'Обед' : 
+(15 < hour < 18) ? 'Магазин открыт' : 
+(18 < hour < 24) ? 'Магази н закрыт':
+(24 < hour < 50) ? 'tt';
