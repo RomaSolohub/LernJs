3a9e9225e2faeafedc32af9ccfbec47d6c939b4b
RomaSolohub <roman30121998@gmail.com>
2021-08-01 10:11:05 +0300

    rest

--- task-1.js
+++ task-1.js
@@ -1,7 +1,6 @@
 // it's link for my site 
 // https://romasolohub.github.io/LernJs/
 let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
-
 if (year < 2015) {
   alert( 'Это слишком рано...' );
 } else if (year > 2015) {
@@ -9,9 +8,8 @@ if (year < 2015) {
 } else {
   alert( 'Верно!' )};
 let userN = prompt('Сколько пальцев у вас на одной руке', '');
-   if (userN < 5) {
-           alert ('маловато')}
-           ;
-    else if (userN > 5){
-         alert ('многовато')};
-    else alert ('Молодец!')
+if (userN < 5) {
+        alert ('маловато');
+} else  if (userN > 5) {
+        alert ('многовато')};
+   alert ('Молодец!');
