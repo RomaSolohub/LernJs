628e3b52c32969573a397288e96ecd40b4b3ab2e
RomaSolohub <roman30121998@gmail.com>
2021-08-01 10:47:34 +0300

    rest

--- task-1.js
+++ task-1.js
@@ -1,10 +1,13 @@
 // it's link for my site 
 // https://romasolohub.github.io/LernJs/
+let result;
 let year = prompt('Какой год крещения Руси?', '');
 if (year == 998) {
-  alert('Верно!');
+  alert('Верно!')
+  result = 1;
 }  else {
-  alert('Не верно')};
+  alert('Не верно')
+  result = 0};
 let hbdTaras = prompt('Год рождения Тараса Шевченко?', '');
 if (hbdTaras == 1814) {
         alert ('Верно! А Вы знаток.');
@@ -12,9 +15,9 @@ if (hbdTaras == 1814) {
         alert ('Увы...Не верно')};
 let god = prompt('Сколько есть заповедей Божьих?', '');
 if (god == 10) {
-    alert ('Верно!Май Бога в серці, блят');
+    alert ('Верно! Май Бога в серці, блят');
 } else {
-    alert ('Не верно. Май Бога в серці, блят')};
+    alert ('Не верно... Май Бога в серці, блят')};
 let yearNow = prompt('Какой сейчас год?', '');
 if (yearNow == 2021) {
     alert ('Верно!Хорошо что хоть это знаете.');
@@ -25,5 +28,5 @@ if (ter == 4) {
     alert ('Верно! Ваш пес доволен!');
 } else {
     alert ('Не верно. Спроси у своего пса) ')};
-let result = 
+alert ("Ваш результат:" result)
     
