fc20943ef8a2c084daaa0039ef30657b1564f405
RomaSolohub <roman30121998@gmail.com>
2021-08-01 09:51:25 +0300

    rt

--- task-1.js
+++ task-1.js
@@ -1,9 +1,9 @@
 // it's link for my site 
 // https://romasolohub.github.io/LernJs/
-let hour = prompt(`Сколько сейчас часов?`,'')
-let messege = (0 < hour < 8) ? 'Магази н закрыт':
-(8 < hour < 13) ? 'Магазин открыт':
-(13 < hour < 15) ? 'Обед' : 
-(15 < hour < 18) ? 'Магазин открыт' : 
-(18 < hour < 24) ? 'Магази н закрыт':
-(24 < hour < 50) ? 'tt';
+let year = prompt(`Какой сейчас год`,'')
+if (year == 2021) {
+    alert ( "Правильно!" );
+    alert ( "Вы такой умный!" );
+  }
+  else 
+    alert('Ты такой болван!')
